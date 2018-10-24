1b58dce08c6383d325f14b686fc718fc369ec47e
Greg Smith <greg.smith3492@gmail.com>
2018-10-24 17:07:21 -0400

    update gitignore

--- fbcomps_cli.py
+++ fbcomps_cli.py
@@ -3,11 +3,11 @@ import pandas as pd
 
 from cosine_sim import PlayerCompSystem as pcs
 
-if __name__=='__main__':
-    df = pd.read_csv('football_total.csv', index_col='Unnamed: 1')
-    recs = PlayerCompSystem(df)
-    while True:
-        os.system('clear')
-        player = input('\nWho would you like to compare?\n')
-        recs.rec_by_users(player)
-        input()
+
+df = pd.read_csv('football_total.csv', index_col='Unnamed: 1')
+recs = pcs(df)
+while True:
+    os.system('clear')
+    player = input('\nWho would you like to compare?\n')
+    recs.rec_by_users(player)
+    input()
