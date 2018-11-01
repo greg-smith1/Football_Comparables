c530fd866ec9333003acf36636ac0f51a076b954
Greg Smith <greg.smith3492@gmail.com>
2018-11-01 16:36:27 -0400

    delete redundant run input

--- cosine_sim.py
+++ cosine_sim.py
@@ -81,12 +81,3 @@ class PlayerCompSystem(object):
                                         distances.flatten()[i].round(5)))
 
 
-
-if __name__=='__main__':
-    df = pd.read_csv('football_total.csv', index_col='Unnamed: 1')
-    recs = PlayerCompSystem(df)
-    while True:
-        os.system('clear')
-        player = input('\nWho would you like to compare?\n')
-        recs.rec_by_users(player)
-        input()
